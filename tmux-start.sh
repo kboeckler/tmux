6c354ac3baf219269e7e8927525fcf5b492b4d4d
Böckler <Kevin.Boeckler@mach.de>
2019-10-09 13:36:55 +0200

    Configured start script for mach

--- tmux-start.sh
+++ tmux-start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-tmux new -s Main -d -n MainWin
-tmux splitw -h -p 50
+tmux new -s Main -d -n Main zsh
+tmux new-window -n WF zsh
+tmux splitw -v -p 50 zsh
+tmux splitw -h -p 50 zsh
+tmux select-pane -t 0
+tmux select-window -t Main
+tmux send-keys -t "WF.0" 'wf' Enter
+tmux send-keys -t "WF.1" 'wfzw' Enter
+tmux send-keys -t "WF.2" 'pamock' Enter
 tmux attach -t Main
