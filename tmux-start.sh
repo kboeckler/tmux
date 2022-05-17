aaaf21d3e963b4121ec3b2cfcbdf8848399cae24
kbo <Kevin.Boeckler@mach.de>
2022-05-17 15:09:26 +0200

    Updated tmux

--- tmux-start.sh
+++ tmux-start.sh
@@ -2,10 +2,16 @@
 tmux new -s Main -d -n Main zsh
 tmux new-window -n WF zsh
 tmux splitw -v -p 50 zsh
+tmux select-pane -t 0
+tmux splitw -h -p 50 zsh
+tmux select-pane -t 2
+tmux splitw -h -p 40 zsh
 tmux splitw -h -p 50 zsh
 tmux select-pane -t 0
 tmux select-window -t Main
 tmux send-keys -t "WF.0" 'wf' Enter
-tmux send-keys -t "WF.1" 'wfzw' Enter
-tmux send-keys -t "WF.2" 'pamock' Enter
+tmux send-keys -t "WF.1" 'kc' Enter
+tmux send-keys -t "WF.2" 'wfzw' Enter
+tmux send-keys -t "WF.3" 'pamock' Enter
+tmux send-keys -t "WF.4" 'ecmmock' Enter
 tmux attach -t Main
